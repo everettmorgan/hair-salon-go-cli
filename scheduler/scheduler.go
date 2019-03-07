2279a469ba0df8da25fb5d220f8989a4a1c15ff0
ejmorgan <admin@ejmorgan.com>
2019-03-06 18:03:30 -0800

    scheduler refactor

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,101 +1,88 @@
 package scheduler
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
 	"regexp"
-	"strconv"
+
 	"../appointment"
 )
 
-type scheduler struct {
-	name string
+// Scheduler manages the program loop and has a pointer to the tracker
+type Scheduler struct {
+	name      string
 	isRunning bool
-	appointments []appointment.Appointment
+	calendar  string
+	tracker   string
 }
 
-// Scheduler Methods 
-
-func Init(name string) *scheduler {
-	s := &scheduler {
-		name: name,
+// New creates a scheduler and returns a pointer
+func New(agentName string) *Scheduler {
+	return &Scheduler{
+		name:      agentName,
 		isRunning: true,
 	}
-
-	for s.IsRunning() {
-		s.DisplayMenu()
-	}
-
-	return s
 }
 
-func (scheduler *scheduler) IsRunning() bool {
-	return scheduler.isRunning
+func (scheduler *Scheduler) displayMenuAndAwaitSelection() {
+	displayText := `=====================
+		Welcome to the Hair Salon Scheduler!
+		What would you like to do?
+		1. View Upcoming Appointments
+		2. Schedule New Appointment
+		3. Exit Application
+		=====================`
+
+	_, option := scheduler.displayThenGetInputOrSelection("option", displayText)
+
+	// TODO: add handleOptionAction method
+	scheduler.handleOptionAction(option)
 }
 
-func (scheduler *scheduler) DisplayMenu() {
-	fmt.Println("=====================")
-	fmt.Println("Select one of the following options:")
-	fmt.Println("1. List all future appointments")
-	fmt.Println("2. Schedule a new appointment")
-	fmt.Println("3. Exit application")
-	fmt.Println("=====================")
+func (scheduler *Scheduler) displayThenGetInputOrSelection(setting string, displayToUser string, options ...string) (valid bool, input string) {
+	var rValid bool
+	var rValue string
 
-	selection := scheduler.getInput("option")
-
-	isDigit, _ := regexp.MatchString("[0-9]{1}", selection)
-
-	if (isDigit) {
-		s, _ := strconv.Atoi(selection)
-		switch s {
-			case 1:
-				scheduler.displayAppointments()
-			case 2:
-				scheduler.addAppointment()
-			case 3:
-				scheduler.isRunning = false
-		}
-	} else {
-		fmt.Println("Please enter a valid option (1-3): ")
-	}
-}
-
-func (scheduler *scheduler) getInput(setting string) string {
-	var value string
-
+	fmt.Println(displayToUser)
 	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
+	userInput, _ := reader.ReadString('\n')
 
 	if setting == "option" {
-		value = string(input[0])
-	} else if setting == "string" {
-		value = string(input)
-	}
+		rValue = userInput
+		rValid = true
 
-	return value
-}
-
-func (scheduler *scheduler) addAppointment() {
-	newAppointment := *appointment.New()
-	scheduler.appointments = append(scheduler.appointments, newAppointment)
-}
-
-func (scheduler *scheduler) displayAppointments() {
-	Tracker := *appointment.Tracker
-	timeslots := Tracker.TimeSlots
-
-	for i := 0; i < len(timeslots); i++ {
-		for j := 0; j < len(timeslots[i]); j++ {
-			appointment := timeslots[i][j].Appointment;
-			if appointment != nil {
-				fmt.Println("------------------")
-				fmt.Println("Customer: ", appointment.Customer)
-				fmt.Println("Services: ", appointment.Service)
-				fmt.Println("Time: ", appointment.Timeslot)
-				fmt.Println("Duration: ", appointment.Duration)
-				fmt.Println("------------------")
-			}
+		for i := 0; i < len(options); i++ {
+			fmt.Println(string(i+1) + options[i])
 		}
+
+		result, _ := regexp.MatchString("^[0-9]$", rValue)
+
+		if !result {
+			rValid = false
+		}
+
+	} else if setting == "string" {
+		rValue = string(input)
+		rValid = false
 	}
+
+	return rValid, rValue
+}
+
+func (scheduler *Scheduler) addAppointment() {
+	questions := map[string]string{
+		"customer": "What's the customer's name?",
+		"service":  "What kind of service is this?",
+		"timeslot": "What time is the appointment? Please use the following format: 12:00AM/PM",
+		"duration": "How long is this appointment?",
+	}
+
+	_, customer := scheduler.displayThenGetInputOrSelection("string", questions["customer"])
+	_, service := scheduler.displayThenGetInputOrSelection("option", questions["service"], "Haircut", "Shampoo", "Custom")
+	_, timeslot := scheduler.displayThenGetInputOrSelection("string", questions["timeslot"])
+	_, duration := scheduler.displayThenGetInputOrSelection("option", questions["duration"], "15", "30", "45", "60")
+
+	newAppointment := *appointment.New(customer, service, timeslot, duration)
+	scheduler.appointments = append(scheduler.appointments, newAppointment)
 }
